863009a9a69640fa294c4a246dd6aab844b16a17
unknown <connorjstack.wrk@gmail.com>
2023-11-10 10:52:59 -0600

    realized I am using an object to call a static method which is unnecessary

--- leetcode-repo/src/test/java/me/practice/SortingTests/SelectSortTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/SelectSortTest.java
@@ -11,9 +11,9 @@ public class SelectSortTest {
 
     @Test
     public void testSort() {
-        SelectSort selectSort = new SelectSort();
         int[] arr = {2, 3, 5, 1, 4};
-        arr = selectSort.sort(arr);
+
+        arr = SelectSort.sort(arr);
 
         int[] expectedArr = {1, 2, 3, 4, 5};
         assertArrayEquals(expectedArr, arr);
