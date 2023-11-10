e4f4cc940794bd8c965507672e1a64d7a51d629a
unknown <connorjstack.wrk@gmail.com>
2023-11-10 11:37:23 -0600

    cleaning test cases

--- leetcode-repo/src/test/java/me/practice/SortingTests/ComparisonSortsTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/ComparisonSortsTest.java
@@ -7,14 +7,21 @@ import org.junit.jupiter.api.Test;
 import me.practice.Sorting.ComparisonSorts;
 
 public class ComparisonSortsTest {
+    public int[] expectedArrSize5 = {1, 2, 3, 4, 5};
+    public int[] randScrambleSize5 = {3, 1, 4, 2, 5};
+    public int[] nonIncreadingSize5 = {5, 4, 3, 2, 1};
+
+
     @Test
-    public void testBubbleSort(){
-        int[] arr = {3, 1, 4, 2, 5};
-        arr = ComparisonSorts.bubbleSort(arr);
+    public void testBubbleSortWithRandomInput(){
+        int[] arr = ComparisonSorts.bubbleSort(randScrambleSize5);
+        assertArrayEquals(expectedArrSize5, arr);
+    }
 
-        int[] expectedArr = {1, 2, 3, 4, 5};
-
-        assertArrayEquals(expectedArr, arr);
+        @Test
+    public void testBubbleSortWithNonIncreadingInput(){
+        int[] arr = ComparisonSorts.bubbleSort(nonIncreadingSize5);
+        assertArrayEquals(expectedArrSize5, arr);
     }
 
     @Test
@@ -25,5 +32,10 @@ public class ComparisonSortsTest {
         int[] expectedArr = {1, 2, 3, 4, 5};
 
         assertArrayEquals(expectedArr, arr);
+    }
+
+    @Test
+    public void testInsertionSort(){
+        int[] arr = {3, 1, 4, 2, 5};
     }
 }
