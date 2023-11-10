4eebbb6681cdf83e877fe811402f2971644606e1
unknown <connorjstack.wrk@gmail.com>
2023-11-09 21:26:58 -0600

    implemented sorting algorithm

--- leetcode-repo/src/main/java/me/practice/Sorting/SelectSort.java
+++ leetcode-repo/src/main/java/me/practice/Sorting/SelectSort.java
@@ -1,11 +1,28 @@
 package me.practice.Sorting;
 
 public class SelectSort {
-    public SelectSort(){
 
-    }
 
     public int add(int a, int b){
         return a + b;
     }
+
+    public static int[] sort(int[] arr){
+        int min_index = 0, temp = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            min_index = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[j] < arr[min_index]){
+                    min_index = j;
+                }
+            }
+            temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+    
+    
 }
--- leetcode-repo/src/test/java/me/practice/SortingTests/SelectSortTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/SelectSortTest.java
@@ -1,5 +1,6 @@
 package me.practice.SortingTests;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,16 @@ public class SelectSortTest {
         int result = selectSort.add(5, 2);
 
         assertEquals(7, result);
+    }
+
+    @Test
+    public void testSort() {
+        SelectSort selectSort = new SelectSort();
+        int[] arr = {2, 3, 5, 1, 4};
+        arr = selectSort.sort(arr);
+
+        int[] expectedArr = {1, 2, 3, 4, 5};
+        assertArrayEquals(expectedArr, arr);
     }
     
 }
