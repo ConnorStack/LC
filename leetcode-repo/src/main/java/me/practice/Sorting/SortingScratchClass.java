1d851731650b3946345ae59b03ec4382190180ef
ConnorStack <connor.stack@g.austincc.edu>
2023-11-12 10:41:55 -0600

    adding practice reps

--- leetcode-repo/src/main/java/me/practice/Sorting/SortingScratchClass.java
+++ leetcode-repo/src/main/java/me/practice/Sorting/SortingScratchClass.java
@@ -17,4 +17,40 @@ public class SortingScratchClass {
         
         return arr;
     }
+
+    public static int[] heapSortPrac(int[] arr){
+        //first heapify the array in-place
+        for (int i = arr.length/2 - 1 ; i >= 0; i--) {
+            maxHeapify(arr, arr.length, i);
+        }
+
+        for (int i = arr.length-1; i > 0; i--) {
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+            maxHeapify(arr, i, 0);
+        }
+
+
+        return arr;
+    }
+
+    private static void maxHeapify(int[] arr, int heapSize, int index){
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        int largest = index;
+
+        if(left < heapSize && arr[left] > arr[largest]){
+            largest = left;
+        }
+        if(right < heapSize && arr[right] > arr[largest]){
+            largest = right;
+        }
+        if(index != largest){
+            int temp = arr[largest];
+            arr[largest] = arr[index];
+            arr[index] = temp;
+            maxHeapify(arr, heapSize, largest);
+        }
+    }
 }
---
+++ leetcode-repo/src/test/java/me/practice/SortingTests/SortingScratchClassTest.java
@@ -0,0 +1,22 @@
+package me.practice.SortingTests;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import me.practice.Sorting.SortingScratchClass;
+
+public class SortingScratchClassTest {
+    public int[] expectedArrSize5 = { 1, 2, 3, 4, 5};
+    public int[] expectedArrSize10 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    public int[] randArraySize5 = { 3, 1, 4, 2, 5 };
+    public int[] randArraySize10 = { 5, 1, 10, 6, 4, 7, 2, 9, 3, 8 };
+
+    public int[] nonIncreadingSize5 = { 5, 4, 3, 2, 1 };
+    @Test
+    public void testHeapSortPrac(){
+        int arr[] = SortingScratchClass.heapSortPrac(randArraySize10);
+        assertArrayEquals(arr, expectedArrSize10);
+    }
+}
