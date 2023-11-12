13779aa44805fe4947f42707f665e22cb12018c2
ConnorStack <connor.stack@g.austincc.edu>
2023-11-12 11:00:07 -0600

    fixing other variable typo

--- leetcode-repo/src/test/java/me/practice/SortingTests/ComparisonSortsTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/ComparisonSortsTest.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.Test;
 import me.practice.Sorting.ComparisonSorts;
 
 public class ComparisonSortsTest {
-    public int[] expectedArrSize5 = { 1, 2, 3, 4, 5 };
+    public int[] expectedArrSize5 = { 1, 2, 3, 4, 5};
+    public int[] expectedArrSize10 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
     public int[] randArraySize5 = { 3, 1, 4, 2, 5 };
-    public int[] nonIncreadingSize5 = { 5, 4, 3, 2, 1 };
+    public int[] randArraySize10 = { 5, 1, 10, 6, 4, 7, 2, 9, 3, 8 };
+
+    public int[] nonIncreasingSize5 = { 5, 4, 3, 2, 1 };
+    public int[] nonIncreasingSize10 = {10, 9,8, 7, 6, 5, 4, 3, 2, 1};
 
     @Test
     public void testBubbleSortWithRandomInput() {
