61ef3fea39c66acdbafb8b52005c988a25ae7640
unknown <connorjstack.wrk@gmail.com>
2023-11-13 10:51:36 -0600

    organizing tests for practice methods

--- leetcode-repo/src/test/java/me/practice/SortingTests/SortingScratchClassTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/SortingScratchClassTest.java
@@ -10,10 +10,10 @@ public class SortingScratchClassTest {
     public int[] expectedArrSize5 = { 1, 2, 3, 4, 5};
     public int[] expectedArrSize10 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    public int[] randCountArray = {0, 0, 4, 2, 4, 5, 2, 1, 0};
-    public int[] randCountArray2 = { 9, 3, 8, 3, 5, 4, 5, 5, 2};    
-    public int[] expectedCountArray = {0, 0, 0, 1, 2, 2, 4, 4, 5};
-    public int[] expectedCountArray2 = {2, 3, 3, 4, 5, 5, 5, 8, 9};
+    public int[] randCountArrayRandomVariation = {0, 0, 4, 2, 4, 5, 2, 1, 0};
+    public int[] randCountArrayNoZeroVariation = { 9, 3, 8, 3, 5, 4, 5, 5, 2};    
+    public int[] expectedCountArrayRandomVariaton = {0, 0, 0, 1, 2, 2, 4, 4, 5};
+    public int[] expectedCountArrayNoZeroVariation = {2, 3, 3, 4, 5, 5, 5, 8, 9};
 
     public int[] randArraySize5 = { 3, 1, 4, 2, 5 };
     public int[] randArraySize10 = { 5, 1, 10, 6, 4, 7, 2, 9, 3, 8 };
@@ -29,7 +29,11 @@ public class SortingScratchClassTest {
 
     @Test
     public void testCountSortPrac(){
-        int arr[] = SortingScratchClass.countSortPrac(randCountArray);
-        assertArrayEquals(expectedCountArray, arr);
+        assertArrayEquals(expectedCountArrayRandomVariaton, SortingScratchClass.countSortPrac(randCountArrayRandomVariation));
+    }
+
+    @Test
+    public void testCountSortPracWithNoZeroVariation(){
+        assertArrayEquals(expectedCountArrayNoZeroVariation, SortingScratchClass.countSortPrac(randCountArrayNoZeroVariation));
     }
 }
