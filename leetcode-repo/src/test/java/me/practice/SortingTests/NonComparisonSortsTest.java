2d72f56ed49f54daf53f10a632498d33e1899fd2
unknown <connorjstack.wrk@gmail.com>
2023-11-15 22:36:31 -0600

    adding additional test cases

--- leetcode-repo/src/test/java/me/practice/SortingTests/NonComparisonSortsTest.java
+++ leetcode-repo/src/test/java/me/practice/SortingTests/NonComparisonSortsTest.java
@@ -8,7 +8,7 @@ import me.practice.Sorting.NonComparisonSorts;
 
 public class NonComparisonSortsTest {
     public int[] expectedArrSize5 = { 0, 1, 2, 3, 4, 5};
-    public int[] expectedArrSize10 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public int[] expectedArrSize10 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     public int[] randCountArray = {0, 0, 4, 2, 4, 5, 2, 1, 0};
     public int[] expectedCountArray = {0, 0, 0, 1, 2, 2, 4, 4, 5};
@@ -20,7 +20,7 @@ public class NonComparisonSortsTest {
     public int[] expectedRadixArray = {132, 243, 286, 316, 399, 591, 869, 912};
 
     public int[] randArraySize5 = { 3, 1, 4, 2, 5, 0};
-    public int[] randArraySize10 = { 5, 1, 10, 6, 4, 7, 2, 9, 3, 8 };
+    public int[] randArraySize10 = { 0, 5, 1, 6, 4, 7, 2, 9, 3, 8 };
 
     public int[] nonIncreasingSize5 = { 5, 4, 3, 2, 1 };
     public int[] nonIncreasingSize10 = {10, 9,8, 7, 6, 5, 4, 3, 2, 1};
@@ -28,6 +28,11 @@ public class NonComparisonSortsTest {
     @Test
     public void testBasicCountSortWithRandomSize5(){
         assertArrayEquals(expectedArrSize5, NonComparisonSorts.basicCountSort(randArraySize5));
+    }
+
+        @Test
+    public void testBasicCountSortWithRandomSize10(){
+        assertArrayEquals(expectedArrSize10, NonComparisonSorts.basicCountSort(randArraySize10));
     }
 
     @Test
