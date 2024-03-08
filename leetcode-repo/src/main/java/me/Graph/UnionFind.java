0cfc0915023a9464512efeef36d89ddc6fcb69e3
Connor Stack <connorstack123@gmail.com>
2024-03-08 11:16:14 -0600

    Implement find() and union()

--- leetcode-repo/src/main/java/me/Graph/UnionFind.java
+++ leetcode-repo/src/main/java/me/Graph/UnionFind.java
@@ -10,4 +10,21 @@ public class UnionFind {
         }
     }
 
+    public int find(int x) {
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
+                }
+            }
+        }
+    }
+
 }
